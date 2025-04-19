d98c3302fc132fe8f0b7b1eb1075acca4f776d4e
dev.rary@gmail.com <dev.rary@gmail.com>
2025-04-19 17:59:16 +0900

    user entity uploaded

---
+++ user-service/src/main/java/com/example/userservice/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.userservice.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="users")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String department;
+}
